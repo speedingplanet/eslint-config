6526f3996fc6dab9a8fd3dd724aacb056f65d8bb
John Paxton <johnpaxton@gmail.com>
2023-10-24 07:24:00 -0400

    Revert "V 0.0.4"

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,28 +1,22 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-env node, mocha */
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { ESLint } from 'eslint';
-import { expect } from 'chai';
-import config from '../eslint.config.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const expect = require('chai').expect;
+const path = require('path');
+const { ESLint } = require('eslint');
 
 describe('Basic ESLint config tests', () => {
-	/** @type ESLint */
 	let linter;
 
 	beforeEach(() => {
-		linter = new ESLint({ useEslintrc: false, baseConfig: config[0] });
+		linter = new ESLint();
 	});
 
 	it('should warn about tabs over spaces', async () => {
 		const code = `
     console.log('This is bad');
 `;
-		let results = await linter.lintText(code);
+		let results = await linter.lintText(code, { filePath: path.resolve(__dirname, '../index.js') });
 		expect(results).not.to.be.null;
 		expect(results.length).to.be.greaterThan(0);
 		let firstRecord = results[0];
