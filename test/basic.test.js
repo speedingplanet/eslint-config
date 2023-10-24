18d41d62b516a1081e4e53e0a415d5a36bbaff99
John Paxton (macos) <john_paxton@tjx.com>
2023-10-24 06:57:26 -0400

    Update to ESM; still not working, docs suck

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,22 +1,28 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-env node, mocha */
 
-const expect = require('chai').expect;
-const path = require('path');
-const { ESLint } = require('eslint');
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { ESLint } from 'eslint';
+import { expect } from 'chai';
+import config from '../eslint.config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 describe('Basic ESLint config tests', () => {
+	/** @type ESLint */
 	let linter;
 
 	beforeEach(() => {
-		linter = new ESLint();
+		linter = new ESLint({ useEslintrc: false, baseConfig: config[0] });
 	});
 
 	it('should warn about tabs over spaces', async () => {
 		const code = `
     console.log('This is bad');
 `;
-		let results = await linter.lintText(code, { filePath: path.resolve(__dirname, '../index.js') });
+		let results = await linter.lintText(code);
 		expect(results).not.to.be.null;
 		expect(results.length).to.be.greaterThan(0);
 		let firstRecord = results[0];
