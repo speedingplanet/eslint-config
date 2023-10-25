344cead8d6edc66ebbd387d917fcc84834e09f98
John Paxton (macos) <john_paxton@tjx.com>
2023-10-24 20:34:24 -0400

    Last of Babel and TypeScript cleanup

--- typescript.js
+++
@@ -1,7 +0,0 @@
-/*
-Done via an "overrides" call, based on the fact
-that standard-with-typescript uses that style, which
-is good enough for me
-*/
-
-module.exports = require('./typescript.eslintrc.json');
